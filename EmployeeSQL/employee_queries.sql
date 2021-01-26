ce52b713c1be98dbf3b888ab05edc26d7ef60e6d
Ronjini Hassan <ronjinihassan@ronjinis-mbp.home>
2021-01-25 21:15:33 -0500

    Final queries

--- EmployeeSQL/employee_queries.sql
+++ EmployeeSQL/employee_queries.sql
@@ -55,3 +55,10 @@ ON employees.emp_no = dept_emp.emp_no
 INNER JOIN departments
 ON dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_name IN ('Sales', 'Development');
+
+-- Question 8: In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name) 
+FROM employees
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC;
